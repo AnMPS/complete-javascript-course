5f20c1b78c1625ee0b7b2940cecff4d7068fe46c
AnM00PS <andres.marinmenco@parivedasolutions.com>
2025-07-23 12:26:14 -0500

    updates

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,32 @@
 'use strict';
+
+const jessica = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+};
+
+function marryPerson(person, newLastName) {}
+
+console.log('Before', jessica);
+
+const jessica2 = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+  family: ['Alice', 'Bob'],
+};
+
+//Shallow copy
+const jessicaCopy = { ...jessica2 };
+jessicaCopy.lastName = 'Davis';
+
+console.log(jessica2, jessicaCopy);
+
+//Deep copy
+
+const jessicaClone = structuredClone(jessica2);
+jessicaClone.lastName = 'Davis';
+jessicaClone.family.push('Mary');
+
+console.log(jessica2, jessicaCopy, jessicaClone);
--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -44,4 +44,135 @@ const restaurant = {
       close: 24,
     },
   },
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(`here's your pasta with: ${ing1}, ${ing2}, and ${ing3}`);
+  },
+  orderPizza: function (mainIngredient, ...otherIngredients) {},
 };
+let [first, , second] = restaurant.categories;
+console.log(first, second);
+
+[first, second] = [second, first];
+console.log(first, second);
+
+const [starter, main] = restaurant.order(2, 0);
+console.log(starter, main);
+
+//nested destructuring
+const nested = [2, 4, [5, 6]];
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+
+//default values
+const [p = 1, q = 1, r = 1] = [8, 9];
+
+console.log(p, q, r);
+
+//deconstruct object
+const { name, openingHours, categories } = restaurant;
+
+const { name: restName, openingHours: hours, categories: tags } = restaurant;
+
+//defaults
+
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
+
+// //mutating variables
+// let a = 111;
+// let b = 222;
+// // const obj = { a: 23, b: 7, c: 14 };
+
+// ({ a, b } = obj);
+
+//nested object
+const {
+  fri: { open, close },
+} = openingHours;
+
+//spread operator
+const arr = [7, 8, 9];
+
+const badNewArray = [1, 2, arr[0], arr[1], arr[2]];
+
+const newArr = [1, 2, ...arr];
+
+const newMenu = [...restaurant.mainMenu, 'Gnocchi'];
+
+const fullMenu = [...restaurant.mainMenu, ...restaurant.starterMenu];
+
+const ingredients = ['cheese', 'egg', 'bacon'];
+console.log(restaurant.orderPasta(...ingredients));
+console.log(restaurant.orderPizza('mushrooms', 'olives', 'spinach'));
+
+///REST
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+
+const [pizza, , risotto, ...otherFood] = [
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
+];
+
+//grabs pizza and risotto from main menu, then adds starter menu as an array at the end and makes them all new variables.
+
+console.log([pizza, risotto, otherFood]);
+
+const { sat, ...weekdays } = restaurant.openingHours;
+
+console.log(sat, weekdays);
+
+const add = function (...numbers) {
+  let sum = 0;
+  for (const x of numbers) {
+    sum += x;
+  }
+  return sum;
+};
+
+//these are the same code because numGuests does not exist.
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+const guests2 = restaurant.numGuests || 10;
+
+//&& short circuits to falsy value or last value
+
+console.log(0 && 'Jonas'); //returns 0
+
+console.log(7 && 'Jonas'); //returns Jonas
+
+console.log('Hello' && 23 && null && 8); //returns null
+
+//these are the same code
+if (restaurant.orderPizza) {
+  restaurant.orderPasta('mushrooms', 'spinach');
+}
+
+restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
+
+const rest1 = {
+  name: 'Capri',
+  numGuests: 20,
+};
+const rest2 = {
+  name: 'La Piazza',
+  owner: 'Giovanni Rossi',
+};
+
+//rest1 10, rest2 20
+rest1.numGuests = rest1.numGuests || 10;
+rest2.numGuests = rest2.numGuests || 10;
+
+//same as above, 10
+rest1.numGuests ||= 10;
+
+//same as above 20
+rest2.numGuests ||= 20;
+
+// when 0 use ?? instead
+rest1.numGuests ??= 10;
+
+//replace if it exists
+rest1.owner &&= 'meme'; //leaves undefined
+rest2.owner &&= 'meme'; //assigns to meme
